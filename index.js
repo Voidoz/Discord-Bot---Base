6fe998e4289abcda9498fac9262a0387cea7f473
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-03-21 11:24:58 +1300

    changed whitespace in index.js

--- index.js
+++ index.js
@@ -21,25 +21,20 @@ client.on('message', message => {
 
     if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
-    }
-    else if (message.content.startsWith(`${prefix}beep`)) {
+    } else if (message.content.startsWith(`${prefix}beep`)) {
         message.channel.send('Boop.');
-    }
-    else if (message.content === `${prefix}server`) {
+    } else if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    }
-    else if (message.content === `${prefix}user-info`) {
+    } else if (message.content === `${prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    }
-    else if (command === 'args-info') {
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        else if (args[0] === 'foo') {
-            return message.channel.send('bar');
-        }
+    } else if (command === 'args-info') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        } else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
 
-        message.channel.send(`First argument: ${args[0]}`);
+		message.channel.send(`First argument: ${args[0]}`);
     }
 });
 
