0ec50f75033b16b5bfaa2ca3ccb3e2577dc2ff4c
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-03-21 10:02:35 +1300

    changes to index code

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ client.once('ready', () => {
 
 // this code is run when the bot detects an incoming message
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
-    } else if (message.content.startsWith(`${prefix}beep`)) {
+    }
+    else if (message.content.startsWith(`${prefix}beep`)) {
         message.channel.send('Boop.');
-    } else if (message.content === `${prefix}server`) {
+    }
+    else if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (message.content === `${prefix}user-info`) {
+    }
+    else if (message.content === `${prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`First argument: ${args[0]}`);
     }
 });
 
