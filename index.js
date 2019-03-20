47a9319c843f61ed37cc7c068e0c47e1a2b3ea7b
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-03-20 15:43:10 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+const { prefix, token } = require('./config.json');
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// this code is run when the bot detects an incoming message
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+        message.channel.send('Pong.');
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('Boop.');
+    } else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    } else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+});
+
+// login to Discord with app's token
+client.login(token);
