c2c5195e82ab8ec010a81294206588eef7262193
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-03-22 12:25:37 +1300

    added help command

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
 
-// const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./config.json');
+
+const commandList = require('./commands.json');
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -23,18 +25,18 @@ client.once('ready', () => {
 
 // this code is run when the bot detects an incoming message
 client.on('message', message => {
-    if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(process.env.prefix.length).split(/ +/);
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (message.content.startsWith(`${process.env.prefix}ping`)) {
+    if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
-    } else if (message.content.startsWith(`${process.env.prefix}beep`)) {
+    } else if (message.content.startsWith(`${prefix}beep`)) {
         message.channel.send('Boop.');
-    } else if (message.content === `${process.env.prefix}server`) {
+    } else if (message.content === `${prefix}server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (message.content === `${process.env.prefix}user-info`) {
+    } else if (message.content === `${prefix}user-info`) {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     } else if (command === 'args-info') {
 		if (!args.length) {
@@ -44,8 +46,10 @@ client.on('message', message => {
 		}
 
 		message.channel.send(`First argument: ${args[0]}`);
+    } else if (message.content === `${prefix}help`) {
+        message.channel.send(`Commands requested by ${message.author}:\n${commandList.commands}\n\n\nPlease note that this version of the bot is case sensetive for the command itself. For example you wrote "^ArGs-InFo HeLlO wOrLd" it would not output anything because "^args-info" includes caps. If you wrote "^args-info HeLlO wOrLd" it would reply with your argument info because "^args-info" does not include caps.`);
     }
 });
 
 // login to Discord with app's token
-client.login(process.env.token);
+client.login(token);
