e3ff0b4f2662b0ebe2469fab4d68bd37e11e5040
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-03-21 15:47:37 +1300

    changes to index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.once('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(' ');
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (message.content.startsWith(`${prefix}ping`)) {
